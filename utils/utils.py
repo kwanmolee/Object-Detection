625744604f4880b17ef9d402df275966263b53bf
kwanmolee <kunmaoli2017@163.com>
2020-03-12 22:17:40 -0700

    update data and utils

--- utils/utils.py
+++ utils/utils.py
@@ -383,11 +383,11 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        summary = tf.compat.v1.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
         self.writer.add_summary(summary, step)
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag = tag, simple_value = value) for tag, value in tag_value_pairs])
+        summary = tf.compat.v1.Summary(value=[tf.Summary.Value(tag = tag, simple_value = value) for tag, value in tag_value_pairs])
         self.writer.add_summary(summary, step)
 
